fbe7ac9e63f26b79755c169118e4b8e3cf28c1fb
Henrik Jürges <juerges.henrik@gmail.com>
2022-02-26 14:53:45 +0100

    added package template

---
+++ clockodo.el
@@ -0,0 +1,21 @@
+;;; clockodo.el --- Integrate the clockodo timer into emacs. -*- lexical-binding: t -*-
+
+;; Copyright (c) Henrik Jürges
+
+;; Author: Henrik Jürges <juerges.henrik@gmail.com>
+;; Version: 0.9
+;; Package-Require: ((example "1.0"))
+;; Keywords: time, organization
+;; URL: https://github.com/santifa/clockodo-el
+
+;;; Commentary:
+
+;; This package provides a minor mode to interact with the clockodo api
+;; with simple commands.
+
+;;; Code:
+
+;; Some code here
+
+(provide 'clockodo)
+;;; clockodo.el ends here
